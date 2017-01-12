32abd2df921e3bb60f42b1542fa089fcfb20aee0
Mike Bryant <mikesname@gmail.com>
2017-01-12 10:42:43 +0000

    More info about items

--- app/models/InformationAsset.scala
+++ app/models/InformationAsset.scala
@@ -7,6 +7,8 @@ import play.api.libs.functional.syntax._
 case class InformationAsset(
   id: String,
   parent: Option[String] = None,
+  title: Option[String] = None,
+  description: Option[String] = None,
   data: JsObject
 )
 
@@ -14,6 +16,8 @@ object InformationAsset {
   implicit val reads: Reads[InformationAsset] = (
     (__ \ "IAID").read[String] and
     (__ \ "ParentIAID").readNullable[String] and
+    (__ \ "Title").readNullable[String] and
+    (__ \ "Description").readNullable[String] and
     __.read[JsObject]
   )(InformationAsset.apply _)
 }
